f1b6b48631cc84ad036f447c0220a53d69eb0e93
Raphael <raphael@seebachers.ch>
2013-05-06 17:08:20 +0200

    Added documentation template. Minor code reordering.

--- lib/AnyEvent/STOMP/Client.pm
+++ lib/AnyEvent/STOMP/Client.pm
@@ -1,17 +1,21 @@
 package AnyEvent::STOMP::Client;
 
+
 use strict;
 use warnings;
+
+use parent 'Object::Event';
 
 use AnyEvent;
 use AnyEvent::Handle;
 use List::Util 'max';
-use parent 'Object::Event';
 
-our $VERSION = 0.02;
+
+our $VERSION = '0.02';
+
 
 my $TIMEOUT_MARGIN = 1000;
-my $EOL = chr(10); # or chr(13).chr(10)
+my $EOL = chr(10);
 my $NULL = chr(0);
 my $HEARTBEAT = '0,0';
 
@@ -337,3 +341,53 @@ sub on_error {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+AnyEvent::STOMP::Client - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use AnyEvent::STOMP::Client;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for AnyEvent::STOMP::Client, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Raphael Seebacher, E<lt>raphael@seebachers.chE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Raphael Seebacher
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
