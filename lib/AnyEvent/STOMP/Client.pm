3896df2501687dfdcb952da1db56fe4c6b9a607f
Raphael Seebacher <raphael@seebachers.ch>
2013-11-27 10:32:36 +0100

    Bumped version number to 0.35 and updated changes.

--- lib/AnyEvent/STOMP/Client.pm
+++ lib/AnyEvent/STOMP/Client.pm
@@ -11,7 +11,7 @@ use AnyEvent::Handle;
 use List::Util 'max';
 
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 
 my $EOL = chr(10);
--- lib/AnyEvent/STOMP/Client/Any.pm
+++ lib/AnyEvent/STOMP/Client/Any.pm
@@ -10,7 +10,7 @@ use Log::Any '$log';
 use Time::HiRes 'time';
 
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 
 my $SEPARATOR_ID_ACK = '#';
